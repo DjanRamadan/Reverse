97992466b337286cb4e8f32a4957f2b8c7e88201
Djan <124775935+DjanRamadan@users.noreply.github.com>
2023-03-01 13:16:16 +0200

    Add files via upload

---
+++ Reverse.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter character1: ");
+var char1 = Convert.ToString(Console.ReadLine());
+Console.Write("Enter character2: ");
+var char2 = Convert.ToString(Console.ReadLine());
+Console.Write("Enter character3: ");
+var char3 = Convert.ToString(Console.ReadLine());
+Console.WriteLine(char3 + char2 + char1);
